77ac0919aa024c85b3bbd96c6817b8c164c7720d
ujiniverse <88028826+ujiniverse@users.noreply.github.com>
2022-02-17 02:46:47 +0900

    BEM

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button1 from './components/button1';
+import Button2 from './components/button2';
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+    </>
+  );
+}
+
+export default App;
---
+++ components/button1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './button1.css';
+
+class Button1 extends Component {
+    render() {
+        return (
+            <div className="button1_button">  //클래스 이름
+                <span className="button1_text">Button1</span>  //클래스 이름
+            </div>
+        );
+    }
+}
+
+export default Button1;
